657e97002379222452bc4463fbd665ce12bbcc6a
A7ig <77713023+A7ig@users.noreply.github.com>
2021-06-16 10:47:17 +0800

    Initial commit

---
+++ src/Tailwindcss.svelte
@@ -0,0 +1,20 @@
+<style global>
+	@tailwind base;
+	
+	body {padding: 0;}
+	h1 {
+	  @apply text-2xl;
+	}
+	h2 {
+	  @apply text-xl;
+	}
+	h3 {
+	  @apply text-lg;
+	}
+	a {
+	  @apply text-blue-600 underline;
+	}
+
+	@tailwind components;
+	@tailwind utilities;
+</style>
